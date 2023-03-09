25f571ba1fdb25cb01579105a6cf30436d9edab8
Marcelo Pimentel <marcelo.ide.pimentel@gmail.com>
2023-03-08 22:01:00 -0300

    Adicionada criação de nova referencia para proteção de objeto privado

--- app/models/negociacao.ts
+++ app/models/negociacao.ts
@@ -1,11 +1,18 @@
 export class Negociacao {
 
   constructor(
-    public readonly data: Date, 
+    private _data: Date, 
     public readonly quantidade: number, 
     public readonly valor: number) {}
 
   get volume(): number {
     return this.quantidade * this.valor;
+  }
+
+  get data(): Date {
+    // retorna uma data com mesma informação
+    // porém de outra referencia para não ser modificada
+    const data = new Date(this._data.getTime());
+    return data;
   }
 }
